e46b8079875a964a851b13d4af67f91c09ac3292
parkoon <devparkoon@gmail.com>
2021-11-02 18:43:44 +0900

    Typescript 설정

---
+++ src/components/Text.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react'
+
+const Text: FunctionComponent = function ({ children }) {
+  return <div>{children}</div>
+}
+
+export default Text
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,11 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+
+
 const IndexPage = () => (
   <Layout>
+
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
