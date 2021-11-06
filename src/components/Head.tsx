2fc34371d70e261093cfd85240f74461320eb4b4
parkoon <devparkoon@gmail.com>
2021-11-06 15:10:49 +0900

    html lang="ko"

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -7,8 +7,8 @@ const Head: FunctionComponent = () => {
 
   return (
     <Helmet>
+      <html lang="ko" />
       <title>{title}</title>
-
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
