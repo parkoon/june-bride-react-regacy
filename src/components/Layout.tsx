189675661b7a49da09b74ab966b1b9d0d0295b94
parkoon <devparkoon@gmail.com>
2021-11-06 22:31:41 +0900

    기본은 모바일 사이즈로 대응

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,18 @@ import styled from '@emotion/styled'
 import GlobalStyle from './GlobalStyle'
 import Head from './Head'
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+    min-height: 100vh;
+    background: rgba(0, 0, 0, 0.05);
+
+    @media (min-width: 1024px) {
+        width: 375px;
+        margin: 0 auto;
+        border: 1px solid gray;
+        border-top: none;
+        border-bottom: none;
+    }
+`
 
 const Layout: FunctionComponent = ({ children }) => (
     <Wrapper>
