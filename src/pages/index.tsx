ae711e8ab486cc446b330a7a4b5fc62cf4112a02
parkoon <devparkoon@gmail.com>
2021-11-06 14:58:09 +0900

    불필요한 페이지 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import React, { FunctionComponent } from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 
 const IndexPage: FunctionComponent = function () {
-  return (
-    <Layout>
-      <Link to="/info/">To Info</Link>
-    </Layout>
-  )
+  return <Layout>Hello World</Layout>
 }
 
 export default IndexPage
