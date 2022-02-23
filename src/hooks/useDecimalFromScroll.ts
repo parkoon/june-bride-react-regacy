070254bb31252af505c1045822d80c1ad517a3ab
parkoon <devparkoon@gmail.com>
2022-02-23 20:08:33 +0900

    fix: 증가/감소에 따른 min max 함수 분기처리

--- src/hooks/useDecimalFromScroll.ts
+++ src/hooks/useDecimalFromScroll.ts
@@ -1,29 +1,31 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 type UseDecimalFromScrollOptions = {
     start?: number
     end?: number
-    increase?: boolean
     intensity?: number
 }
 function useDecimalFromScroll({
     start = 1,
     end = 100,
-    increase = true,
     intensity = 1,
 }: UseDecimalFromScrollOptions = {}) {
     const [value, setValue] = useState(start)
+
+    const isIncrement = useMemo(() => start - end < 0, [start, end])
+
     useEffect(() => {
         const handleScroll = () => {
             const scroll =
                 window.pageYOffset || document.documentElement.scrollTop
 
-            const amount = (scroll * intensity) / 100
+            const amount = isIncrement
+                ? (scroll * intensity) / 100
+                : (-scroll * intensity) / 100
 
-            const result = Math.min(
-                (start * 100 + (increase ? amount : -amount)) / 100,
-                end
-            )
+            const minMax = isIncrement ? Math.min : Math.max
+
+            const result = minMax((start * 100 + amount) / 100, end)
 
             setValue(result)
         }
