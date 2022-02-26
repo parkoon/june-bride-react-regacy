f9b8a647147fd1566c044ada63d1bfddbc9f44cc
parkoon <devparkoon@gmail.com>
2022-02-26 23:20:34 +0900

    앱 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Date from './components/Date'
 import Landing from './components/Landing'
 import Layout from './components/Layout'
+import When from './components/When'
 
 function App() {
     return (
         <Layout>
-            <div>
+            {/* <div>
                 <Landing />
             </div>
 
@@ -37,8 +39,47 @@ function App() {
                     dolore aut in, eum ex! Repellendus quas assumenda sed quasi
                     consequuntur?
                 </p>
+            </section> */}
+            <Landing />
+            <When />
+            <section
+                style={{
+                    textAlign: 'center',
+                    padding: '0 20px',
+                }}
+            >
+                <h1>Lorem ipsum </h1>
+                <p>
+                    dolor sit amet consectetur adipisicing elit. Rerum dolores
+                    debitis numquam neque consequuntur sed saepe at minus quod
+                    dolore aut in, eum ex! Repellendus quas assumenda sed quasi
+                    consequuntur?
+                </p>
+                <br />
+                <p>
+                    dolor sit amet consectetur adipisicing elit. Rerum dolores
+                    debitis numquam neque consequuntur sed saepe at minus quod
+                    dolore aut in, eum ex! Repellendus quas assumenda sed quasi
+                    consequuntur?
+                </p>
+                <br />
+                <p>
+                    dolor sit amet consectetur adipisicing elit. Rerum dolores
+                    debitis numquam neque consequuntur sed saepe at minus quod
+                    dolore aut in, eum ex! Repellendus quas assumenda sed quasi
+                    consequuntur?
+                </p>
+                <br />
+                <p>
+                    dolor sit amet consectetur adipisicing elit. Rerum dolores
+                    debitis numquam neque consequuntur sed saepe at minus quod
+                    dolore aut in, eum ex! Repellendus quas assumenda sed quasi
+                    consequuntur?
+                </p>
             </section>
+            {/* <Landing />
 
+            <Date /> */}
             {/* <FakeLoadingProvider>
                 <Welcome />
                 <WelcomeMessage />
