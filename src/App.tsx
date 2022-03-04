7f264f7c315547555c53565fb3dd8fef13d5ea69
parkoon <devparkoon@gmail.com>
2022-03-04 10:30:51 +0900

    chore: kakao map test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 import Landing from './components/Landing'
 import Layout from './components/Layout'
 import Map from './components/Map'
+import MapKaKao from './components/Map/MapKaKao'
 import When from './components/When'
 
 const EmptyBox = styled.div<{ opacity: number }>`
@@ -22,6 +23,9 @@ function App() {
             <Map />
             {/* <BankAccount /> */}
             <EmptyBox opacity={0.2} />
+
+            <MapKaKao visible />
+
             <EmptyBox opacity={0.3} />
             {/* <Landing />
 
