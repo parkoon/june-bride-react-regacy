bab0cd370a9e91721f87f612547f97e2420ae63e
parkoon <devparkoon@gmail.com>
2022-03-04 10:09:06 +0900

    feat: add reverse

--- src/components/@common/SectionParagraph.tsx
+++ src/components/@common/SectionParagraph.tsx
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled'
 
-const SectionParagraph = styled.p`
-    font-size: 17px;
+const SectionParagraph = styled.p<{ reverse?: boolean }>`
+    font-size: 15px;
     line-height: 24px;
     margin-bottom: 4px;
+    color: ${({ reverse }) => (reverse ? '#fff' : 'rgb(51, 61, 75)')};
 `
 
 export default SectionParagraph
--- src/components/@common/SectionTitle.tsx
+++ src/components/@common/SectionTitle.tsx
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled'
 
-const SectionTitle = styled.h3`
+const SectionTitle = styled.h3<{ reverse?: boolean }>`
     font-weight: bold;
     font-size: 28px;
     margin-bottom: 13px;
     line-height: 38px;
+    color: ${({ reverse }) => (reverse ? '#fff' : 'rgb(25, 31, 40))')};
 `
 export default SectionTitle
