dc48c2e7135d7e69c189f203ab401b270718aad2
parkoon <devparkoon@gmail.com>
2022-03-13 00:51:32 +0900

    폰트 사이즈 변경

--- src/components/@common/Typo/Paragraph.tsx
+++ src/components/@common/Typo/Paragraph.tsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled'
 
 const Paragraph = styled.p<{ reverse?: boolean }>`
-    font-size: 17px;
+    font-size: 15px;
     line-height: 20px;
     margin-bottom: 4px;
     font-weight: 400;
     color: ${({ reverse }) => (reverse ? '#fff' : 'rgb(51, 61, 75)')};
+
+    margin-bottom: 25px;
 `
 
 export default Paragraph
