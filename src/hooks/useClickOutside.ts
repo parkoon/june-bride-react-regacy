ef8216508fba27f4edaa24d4b9950aae4fa05444
parkoon <devparkoon@gmail.com>
2022-03-13 20:36:53 +0900

    useClickOutside custom hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,18 @@
+import { useEffect, RefObject } from 'react'
+
+function useClickOutside<T extends HTMLElement>(
+    ref: RefObject<T>,
+    callback: () => void
+) {
+    const handleClick = (e: MouseEvent) => {
+        if (ref.current && !ref.current.contains(e.target as Node)) {
+            callback()
+        }
+    }
+    useEffect(() => {
+        document.addEventListener('click', handleClick)
+        return () => document.removeEventListener('click', handleClick)
+    })
+}
+
+export default useClickOutside
